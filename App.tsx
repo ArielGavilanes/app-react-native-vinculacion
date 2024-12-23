d14dcf41b75183897acd6ea581943c6b61e42856
arielGs_Dev <arielogavilanes1534@gmail.com>
2024-12-23 10:40:33 -0500

    configuracion de tabs

--- App.tsx
+++ App.tsx
@@ -1,22 +1,110 @@
-import { View } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import './global.css';
 import { RootStackParamList } from './src/types/RoutingTypes';
 import { HomeScreen } from './src/screens/HomeScreen';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { HomeTabParamList } from './src/types/TabsTypes';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { NavigationContainer } from '@react-navigation/native';
+import { CatalogueScreen } from './src/screens/CatalogueScreen';
+import { CartScreen } from './src/screens/CartScreen';
 
 export const App = () => {
   const Stack = createNativeStackNavigator<RootStackParamList>();
+  const Tab = createBottomTabNavigator<HomeTabParamList>();
+
   const NavigationStack = () => {
     return (
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Navigator screenOptions={{ headerShown: true }}>
+        <Stack.Screen
+          name="Tabs"
+          component={Tabs}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     );
   };
 
+  const Tabs = () => {
+    return (
+      <Tab.Navigator
+        screenOptions={{
+          headerShown: true,
+          tabBarStyle: {
+            backgroundColor: 'white',
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20,
+            borderTopWidth: 1,
+            borderTopColor: 'black',
+          },
+          tabBarActiveTintColor: 'black',
+          tabBarInactiveTintColor: 'black',
+          animation: 'shift',
+        }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarLabel: 'Inicio',
+            tabBarIcon: ({ size, focused }) => (
+              <Ionicons
+                name={focused ? 'home' : 'home-outline'}
+                size={size}
+                color="black"
+                style={{
+                  transform: [{ scale: focused ? 1.2 : 1 }],
+                }}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Catalogue"
+          component={CatalogueScreen}
+          options={{
+            tabBarLabel: 'Catalogo',
+            tabBarIcon: ({ size, focused }) => (
+              <Ionicons
+                name={focused ? 'book' : 'book-outline'}
+                size={size}
+                color="black"
+                style={{
+                  transform: [{ scale: focused ? 1.2 : 1 }],
+                }}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Cart"
+          component={CartScreen}
+          options={{
+            tabBarLabel: 'Carrito',
+            tabBarIcon: ({ size, focused }) => (
+              <Ionicons
+                name={focused ? 'cart' : 'cart-outline'}
+                size={size}
+                color="black"
+                style={{
+                  transform: [{ scale: focused ? 1.2 : 1 }],
+                }}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    );
+  };
+
   return (
-    <View>
-      <NavigationStack />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }} edges={['top']}>
+        <NavigationContainer>
+          <NavigationStack />
+        </NavigationContainer>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 };
---
+++ src/utils/colors.ts
@@ -0,0 +1,3 @@
+export const colors = {
+  primary: '#1E1E1E',
+};
