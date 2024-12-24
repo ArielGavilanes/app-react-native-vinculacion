9b62ddb59b1132dc610281443b7d26dbd6d2c998
arielGs_Dev <90698624+ArielGavilanes@users.noreply.github.com>
2024-12-24 10:43:50 -0500

    Revert "Develop paul"

--- App.tsx
+++ App.tsx
@@ -1,111 +1,21 @@
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
-import './global.css';
-import { RootStackParamList } from './src/types/RoutingTypes';
-import { HomeScreen } from './src/screens/HomeScreen';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { HomeTabParamList } from './src/types/TabsTypes';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import { NavigationContainer } from '@react-navigation/native';
-import { CatalogueScreen } from './src/screens/CatalogueScreen';
-import { CartScreen } from './src/screens/CartScreen';
-import { colors } from './src/utils/colors';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import "./global.css";
 
 export const App = () => {
-  const Stack = createNativeStackNavigator<RootStackParamList>();
-  const Tab = createBottomTabNavigator<HomeTabParamList>();
-
-  const NavigationStack = () => {
-    return (
-      <Stack.Navigator screenOptions={{ headerShown: true }}>
-        <Stack.Screen
-          name="Tabs"
-          component={Tabs}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    );
-  };
-
-  const Tabs = () => {
-    return (
-      <Tab.Navigator
-        screenOptions={{
-          headerShown: true,
-          tabBarStyle: {
-            backgroundColor: 'white',
-            borderTopLeftRadius: 20,
-            borderTopRightRadius: 20,
-            borderTopWidth: 1,
-            borderTopColor: 'black',
-          },
-          tabBarActiveTintColor: 'black',
-          tabBarInactiveTintColor: 'black',
-          animation: 'shift',
-        }}
-      >
-        <Tab.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{
-            tabBarLabel: 'Inicio',
-            tabBarIcon: ({ size, focused }) => (
-              <Ionicons
-                name={focused ? 'home' : 'home-outline'}
-                size={size}
-                color={colors.tertiary}
-                style={{
-                  transform: [{ scale: focused ? 1.2 : 1 }],
-                }}
-              />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Catalogue"
-          component={CatalogueScreen}
-          options={{
-            tabBarLabel: 'Catalogo',
-            tabBarIcon: ({ size, focused }) => (
-              <Ionicons
-                name={focused ? 'book' : 'book-outline'}
-                size={size}
-                color={colors.tertiary}
-                style={{
-                  transform: [{ scale: focused ? 1.2 : 1 }],
-                }}
-              />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Cart"
-          component={CartScreen}
-          options={{
-            tabBarLabel: 'Carrito',
-            tabBarIcon: ({ size, focused }) => (
-              <Ionicons
-                name={focused ? 'cart' : 'cart-outline'}
-                size={size}
-                color={colors.tertiary}
-                style={{
-                  transform: [{ scale: focused ? 1.2 : 1 }],
-                }}
-              />
-            ),
-          }}
-        />
-      </Tab.Navigator>
-    );
-  };
-
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1 }} edges={['top']}>
-        <NavigationContainer>
-          <NavigationStack />
-        </NavigationContainer>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
