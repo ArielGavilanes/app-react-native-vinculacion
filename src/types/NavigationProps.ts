674738cde48bf560ac89dad241f204eab10f9f12
Ariel Gavilanes <arielogavilanes1534@gmail.com>
2024-12-30 08:29:13 -0500

    cart context 1/2

--- src/types/NavigationProps.ts
+++ src/types/NavigationProps.ts
@@ -1,4 +1,11 @@
+import { CatalogueScreen } from './../screens/CatalogueScreen';
+import { CartScreen } from './../screens/CartScreen';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from './RoutingTypes';
 
 export type HomeScreen = NativeStackNavigationProp<RootStackParamList, 'Home'>;
+export type CartScreen = NativeStackNavigationProp<RootStackParamList, 'Cart'>;
+export type CatalogueScreen = NativeStackNavigationProp<
+  RootStackParamList,
+  'Catalogue'
+>;
--- src/types/RoutingTypes.ts
+++ src/types/RoutingTypes.ts
@@ -1,4 +1,6 @@
 export type RootStackParamList = {
   Home: undefined;
   Tabs: undefined;
+  Cart: undefined;
+  Catalogue: undefined;
 };
