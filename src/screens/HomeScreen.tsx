9e2c62448bcc967165804fa642f7dd7b717f4c19
xavierGMFT <axc.llumiquinga@yavirac.edu.ec>
2025-01-05 16:52:45 -0500

    Agregado la imagen de fondo

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,15 @@
-import { View, Text, TouchableOpacity, Image } from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Image,
+  ImageBackground,
+} from 'react-native';
 import { useLayoutEffect } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { Linking } from 'react-native';
-import { colors } from '../utils/colors'; // Importa tu archivo de colores
+import { colors } from '../utils/colors';
 
-// Variables externas para los textos de los botones
 const BUTTON_TEXT = 'IR A VER';
 
 export const HomeScreen = () => {
@@ -33,50 +38,61 @@ export const HomeScreen = () => {
   };
 
   return (
-    <View className="flex-1 bg-gray-100 justify-end pb-8">
-      <View className="flex-row justify-around mb-4">
-        {/* Tarjeta Spa */}
-        <View className="bg-white rounded-lg p-4 items-center w-1/2 shadow-lg">
-          <Image
-            // eslint-disable-next-line @typescript-eslint/no-require-imports
-            source={require('../../assets/productos.png')}
-            className="w-20 h-20 mb-2"
-          />
-          <Text className="text-lg font-bold text-center mb-2">PRODUCTOS</Text>
-          <Text className="text-sm text-gray-600 text-center mb-3">
-            Disfruta de los productos que tenemos para ti
-          </Text>
-          <TouchableOpacity
-            className="bg-purple-300 rounded-full py-2 px-4"
-            onPress={() => openExternalUrl('https://example.com/spa')}
-          >
-            <Text className="text-white font-bold text-center">
-              {BUTTON_TEXT}
+    <ImageBackground
+      // eslint-disable-next-line @typescript-eslint/no-require-imports
+      source={require('../../assets/home2.jpg')}
+      style={{ flex: 1 }}
+      resizeMode="cover"
+    >
+      <View className="flex-1 justify-end pb-8">
+        <View className="flex-row justify-around mb-4">
+          {/* Tarjeta Productos */}
+          <View className="bg-white rounded-lg p-3 items-center w-2/5 shadow-lg">
+            <Image
+              // eslint-disable-next-line @typescript-eslint/no-require-imports
+              source={require('../../assets/productos.png')}
+              className="w-16 h-16 mb-2"
+            />
+            <Text className="text-lg font-bold text-center mb-2">
+              PRODUCTOS
             </Text>
-          </TouchableOpacity>
-        </View>
+            <Text className="text-sm text-gray-600 text-center mb-3">
+              Disfruta de los productos que tenemos para ti
+            </Text>
+            <TouchableOpacity
+              className="bg-purple-300 rounded-full py-2 px-3"
+              onPress={() => openExternalUrl('https://expo.dev/')}
+            >
+              <Text className="text-black font-bold text-center">
+                {BUTTON_TEXT}
+              </Text>
+            </TouchableOpacity>
+          </View>
 
-        {/* Tarjeta Academia */}
-        <View className="bg-white rounded-lg p-4 items-center w-1/2 shadow-lg">
-          <Image
-            // eslint-disable-next-line @typescript-eslint/no-require-imports
-            source={require('../../assets/servicios.png')}
-            className="w-20 h-20 mb-2"
-          />
-          <Text className="text-lg font-bold text-center mb-2">SERVICIOS</Text>
-          <Text className="text-sm text-gray-600 text-center mb-3">
-            Disfruta de los servicios que tenemos para ti
-          </Text>
-          <TouchableOpacity
-            className="bg-purple-300 rounded-full py-2 px-4"
-            onPress={() => openExternalUrl('https://example.com/academy')}
-          >
-            <Text className="text-white font-bold text-center">
-              {BUTTON_TEXT}
+          {/* Tarjeta Servicios */}
+          <View className="bg-white rounded-lg p-3 items-center w-2/5 shadow-lg">
+            <Image
+              // eslint-disable-next-line @typescript-eslint/no-require-imports
+              source={require('../../assets/servicios.png')}
+              className="w-16 h-16 mb-2"
+            />
+            <Text className="text-lg font-bold text-center mb-2">
+              SERVICIOS
             </Text>
-          </TouchableOpacity>
+            <Text className="text-sm text-gray-600 text-center mb-3">
+              Disfruta de los servicios que tenemos para ti
+            </Text>
+            <TouchableOpacity
+              className="bg-purple-300 rounded-full py-2 px-3"
+              onPress={() => openExternalUrl('https://example.com/academy')}
+            >
+              <Text className="text-black font-bold text-center">
+                {BUTTON_TEXT}
+              </Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
-    </View>
+    </ImageBackground>
   );
 };
